d091f85d73569958c05952b3a85be1363ef8f3cc
OrcClaSH <orcaclash@gmail.com>
2023-04-14 16:18:27 +0900

    feat: add errorBoundary in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -6,11 +6,15 @@ import './config/configureMobX'
 import App from './App'
 
 import '@/assets/scss/main.scss';
+import { ErrorBoundary } from 'react-error-boundary';
+import ErrorPage from './App/pages/ErrorPage/ErrorPage';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   // <React.StrictMode>
-    <BrowserRouter>
+  <BrowserRouter>
+    <ErrorBoundary fallback={<ErrorPage />}>
       <App />
-    </BrowserRouter>
+    </ErrorBoundary>
+  </BrowserRouter>
   // </React.StrictMode>,
 )
